ece362e04a9b36ad32946da8f45c69f9722443e5
Jblackmon42 <josh.blackmon42@gmail.com>
2025-07-07 19:31:00 -0500

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>Welcome to Spartan Property Management</h1>
-      <p>Managing multi- and single-family homes across Fort Worth and the surrounding metroplex.</p>
-    </div>
-  );
-}
